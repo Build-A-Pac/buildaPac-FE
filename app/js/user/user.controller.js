15a354ea0058f4f75357d9e4046b4dd70ebab0ea
Max Hewett <mjhewett01@gmail.com>
2015-07-16 10:12:29 -0400

    initial commit

---
+++ app/js/user/user.controller.js
@@ -0,0 +1,34 @@
+;(function () {
+
+  'use strict';
+
+  angular.module('app')
+
+  .controller('UserCtrl', ['$scope', 'UserFactory', '$location',
+
+    function ($scope, UserFactory, $location) {
+      $scope.registerUser = function (userObj) {
+        UserFactory.register(userObj);
+      };
+      $scope.loginUser = function (userObj) {
+        UserFactory.login(userObj);
+      };
+
+
+    }
+
+    ])
+
+.directive('logOut', function (UserFactory) {
+  return {
+    link: function ($scope, element, attrs) {
+      element.bind('click', function (){
+        UserFactory.logOut();
+      });
+    }
+  }
+
+});
+
+
+}());
