7a614ddaba14c9fa125724f5431731795107ac22
Max Hewett <mjhewett01@gmail.com>
2015-07-22 14:44:38 -0400

    working on autobuild. trying to get total price to show up. can get the parts showing up.

--- app/js/autobuild/autobuild.controller.js
+++ app/js/autobuild/autobuild.controller.js
@@ -6,22 +6,45 @@
 
   .controller('AutoCtrl', function ($scope, $http, HEROKU, $stateParams) {
 
-    $scope.autoBuild = [];
+
+
+$scope.autoBuild = function (total_price) {
+
+  var price = Number(total_price);
+
+
+    $http.post(HEROKU.URL + 'computer/build', {max_price: price},  HEROKU.CONFIG)
+      .success( function (data) {
+console.log(data);
+
+    // $scope.totalPrice = data;
+    $scope.autoBuild = data.parts;
+
+    console.log(data.parts);
+
+    // [{
+    //   total_price: data.total_price,
+    //   parts: data.parts,
+    //   cpu: data.parts[0]
+
+    // }];
+
+
+    // $scope.cpu = data.parts.cpu;
+
+    });
+
+};
+
 
 
   });
 
 
 
-$scope.autoBuild = function (total_price) {
-
-  $http.post(HEROKU.URL + 'computer/build', max_price, HEROKU.CONFIG)
-    .success( function () {
 
 
 
-    })
-}
 
 
 
